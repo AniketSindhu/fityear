374bc84eb8e5a10a3d6a99f0221c087069849f08
KevalPrajapati <keval.prajapatimus@gmail.com>
2021-01-03 14:15:38 +0530

    pedometer implementation

--- lib/UI/pages/homepage.dart
+++ lib/UI/pages/homepage.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:fityear/Methods/Functions/getUser.dart';
 import 'package:fityear/Methods/Functions/logout.dart';
 import 'package:fityear/Models/userModel.dart';
 import 'package:fityear/UI/pages/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:pedometer/pedometer.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -15,15 +18,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   UserModel user;
-  void getUserNow() async{
+  void getUserNow() async {
     user = await getUser();
     setState(() {});
   }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getUserNow();
+    initPlatformState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,55 +42,110 @@ class _HomePageState extends State<HomePage> {
         offset: 0.00,
         blendMode: BlendMode.darken,
         child: ZStack([
-          Lottie.asset('assets/fireworks.json', repeat: false, fit: BoxFit.cover)
-            .h(context.percentHeight*80).centered(),
+          Lottie.asset('assets/fireworks.json',
+                  repeat: false, fit: BoxFit.cover)
+              .h(context.percentHeight * 80)
+              .centered(),
           VStack(
             [
               15.heightBox,
               RichText(
                 text: TextSpan(
                   text: 'F',
-                  style: TextStyle(fontSize: 60,color: Colors.pink[300],fontWeight: FontWeight.w900),
+                  style: TextStyle(
+                      fontSize: 60,
+                      color: Colors.pink[300],
+                      fontWeight: FontWeight.w900),
                   children: <TextSpan>[
-                    TextSpan(text: 'it', style: TextStyle(fontWeight: FontWeight.w900,color: Colors.orange[200])),
-                    TextSpan(text: 'Y',),
-                    TextSpan(text: 'ear', style: TextStyle(fontWeight: FontWeight.w900,color: Colors.orange[200])),
+                    TextSpan(
+                        text: 'it',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.orange[200])),
+                    TextSpan(
+                      text: 'Y',
+                    ),
+                    TextSpan(
+                        text: 'ear',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.orange[200])),
                   ],
                 ),
               ),
               5.heightBox,
-              "236 days remaining".text.medium.size(16).color(Color(0xFFFDB10C)).make().centered(),
+              "236 days remaining"
+                  .text
+                  .medium
+                  .size(16)
+                  .color(Color(0xFFFDB10C))
+                  .make()
+                  .centered(),
               5.heightBox,
-              IconButton(icon: Icon(Icons.logout),color: Colors.red, onPressed:()async{
-                await logout();
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Login()));
-              },iconSize: 30,splashColor: Colors.red),
+              IconButton(
+                  icon: Icon(Icons.logout),
+                  color: Colors.red,
+                  onPressed: () async {
+                    await logout();
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Login()));
+                  },
+                  iconSize: 30,
+                  splashColor: Colors.red),
               Column(
                 children: [
                   CircularPercentIndicator(
-                      radius: 250.0,
-                      lineWidth: 18.0,
-                      percent: 0.7,
-                      backgroundColor: Colors.white,
-                      center: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 65,
-                            backgroundColor: Colors.transparent,
-                            child: user==null||user.image==null?Image.asset('assets/no_image.png',fit: BoxFit.fill,):Image.network(user.image,fit: BoxFit.cover,),
-                          ).centered(),
-                          10.heightBox,
-                          "Normie".text.color(Color(0xFFB283F0)).size(22).bold.make()
-                        ],mainAxisAlignment: MainAxisAlignment.center,
-                      ),
-                      progressColor: Color(0xff0CFDCA),
-                    ).centered(),
+                    radius: 250.0,
+                    lineWidth: 18.0,
+                    percent: 0.7,
+                    backgroundColor: Colors.white,
+                    center: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 65,
+                          backgroundColor: Colors.transparent,
+                          child: user == null || user.image == null
+                              ? Image.asset(
+                                  'assets/no_image.png',
+                                  fit: BoxFit.fill,
+                                )
+                              : Image.network(
+                                  user.image,
+                                  fit: BoxFit.cover,
+                                ),
+                        ).centered(),
+                        10.heightBox,
+                        "Normie"
+                            .text
+                            .color(Color(0xFFB283F0))
+                            .size(22)
+                            .bold
+                            .make()
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    progressColor: Color(0xff0CFDCA),
+                  ).centered(),
                   10.heightBox,
-                  "9315".text.extraBold.size(60).white.makeCentered(),
-                  "Steps".text.medium.size(22).color(Color(0xff0CFDCA)).make().centered(),
-                ],mainAxisAlignment: MainAxisAlignment.center,
+                  _steps.text.extraBold.size(60).white.makeCentered(),
+                  "Steps"
+                      .text
+                      .medium
+                      .size(22)
+                      .color(Color(0xff0CFDCA))
+                      .make()
+                      .centered(),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
               ).expand(),
-              "Leaderboard".text.underline.red500.size(22).medium.make().objectBottomCenter()
+              "Leaderboard"
+                  .text
+                  .underline
+                  .red500
+                  .size(22)
+                  .medium
+                  .make()
+                  .objectBottomCenter()
             ],
             alignment: MainAxisAlignment.start,
             crossAlignment: CrossAxisAlignment.center,
@@ -93,5 +154,51 @@ class _HomePageState extends State<HomePage> {
         ]),
       ),
     );
+  }
+
+//Pedometer methods
+  Stream<StepCount> _stepCountStream;
+  Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '0';
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,33 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fityear/UI/pages/intro_screens.dart';
 import 'package:velocity_x/velocity_x.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: "FitYear",
+              home: Intro(),
+              debugShowCheckedModeBanner: false,
+            );
+          }
           return MaterialApp(
-            title: "FitYear",
-            home: Intro(),
             debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              backgroundColor: Colors.black,
+              body: CircularProgressIndicator().centered(),
+            ),
           );
-        }
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(backgroundColor: Colors.black,body: CircularProgressIndicator().centered(),),
-        );
-      }
-    );
+        });
   }
 }
